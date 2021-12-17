275f09d7d23a00edba4a3a1d188e9d055070aff3
me is noob <79394737+Devansh20055@users.noreply.github.com>
2021-12-17 19:37:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ CaptchaDB = {}
 
 @CaptchaBot.on_message(filters.command("start"))
 async def start_handler(_, event: Message):
-    await event.reply_text("Hi, I am captcha bot by@team_silent_king.")
+    await event.reply_text("Hi, I am captcha bot by @team_silent_king.")
 
 
 @CaptchaBot.on_chat_member_updated()
